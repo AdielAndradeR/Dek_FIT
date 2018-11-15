0ceda3eee5ee0f2c9d74086d63191b53ed4d9bb7
AdielAndrade <adiel.rocha@ccc.ufcg.edu.br>
2018-11-15 15:28:33 -0200

    handling errors and put a npm script for start nodemon

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const app =  express();
+const morgan = require('morgan');
 
 const training = require('./api/routes/training');
 const aerobic = require('./api/routes/aerobic');
 
-app.use('/training' , training);
+app.use(morgan('dev'));
 
+
+app.use('/training' , training);
 app.use('/aerobic' , aerobic);
+
+//handling errors
+
+app.use((req,res,next) => {
+    const err = new Error('Not found');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err , req, res, next) =>{
+    res.status(err.status || 500);
+    res.json({
+        "error" :{
+            message: err.message
+        }
+    });
+});
 
 module.exports = app;
 
